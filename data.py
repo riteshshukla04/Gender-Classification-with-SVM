6b1207603968398b40ba6217ed021eea11ee7ec5
riteshshukla04 <75062358+riteshshukla04@users.noreply.github.com>
2021-12-08 02:07:13 +0530

    Update data.py

--- data.py
+++ data.py
@@ -4,6 +4,9 @@ import cv2
 import matplotlib.pyplot as plt 
 import pickle
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 dir='D:\\web\\Images'
 data=[]
 genders=['male','female']
@@ -20,8 +23,29 @@ for gender in genders:
         except Exception as e:
             pass
 
+        
+count_male=0
+count_female=0  
+for i in data:
+    if i[1]==0:
+        count_male+=1
+    else:
+        count_female+=1
+print(count_male)
+dataset={
+    "male":count_male,
+    "female":count_female
+
+}
+gender = list(dataset.keys())
+values = list(dataset.values())
+plt.bar(gender, values, color ='maroon',
+        width = 0.4)
+plt.show()
+
+
+
 pick_in=open('data1.pickle','wb')
 pickle.dump(data,pick_in)
 pick_in.close()
-        
     
